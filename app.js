9a5cc491627ae7f1596ee31b88adec02ac3d3e15
Aausaf Alam <aausafalam585@gmail.com>
2022-08-28 05:04:18 -0700

    app

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')
+
 const { ConnectDB } = require('./Database')
 const { appRoutes } = require('./routes/dictionary')
 const cors = require('cors');
-const port = process.env.PORT || 8080
+const port = process.env.PORT||8080
 
 
 const app = express()
